41e024cbb4f3d1e071770361cc8f46fa09d23c72
viniciusrvieira <viniciusr.vieira@hotmail.com>
2024-03-10 10:44:11 -0300

    fix: types

--- src/core/types/route.d.ts
+++ src/core/types/route.d.ts
@@ -1,8 +1,7 @@
-import { Methods } from "../config";
 import { RequestHandler } from "express";
 
 export interface IRoute {
-  method: Methods;
+  method: "get" | "post" | "put" | "patch" | "delete";
   path: string;
   middlewares: RequestHandler[];
   handlerName: string | symbol;
